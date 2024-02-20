270d41efd77a997eef381cee498dfd63e64eb0b0
Kento Uchida <21z1016@stu.meisei-u.ac.jp>
2024-02-20 16:05:25 +0900

    微調整

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 $('.slider').slick({
-  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-  slidesToShow: 3,//スライドを画面に3枚見せる
-  slidesToScroll: 3,//1回のスクロールで3枚の写真を移動して見せる
-  dots: true,//下部ドットナビゲーションの表示
+    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+    slidesToShow: 3,//スライドを画面に3枚見せる
+    slidesToScroll: 3,//1回のスクロールで3枚の写真を移動して見せる
+    dots: true,//下部ドットナビゲーションの表示
 
-  responsive: [
+    responsive: [
+        {
+        breakpoint: 769,//モニターの横幅が769px以下の見せ方
+        settings: {
+          slidesToShow: 2,//スライドを画面に2枚見せる
+          slidesToScroll: 2,//1回のスクロールで2枚の写真を移動して見せる
+        }
+      },
       {
-      breakpoint: 769,//モニターの横幅が769px以下の見せ方
-      settings: {
-        slidesToShow: 2,//スライドを画面に2枚見せる
-        slidesToScroll: 2,//1回のスクロールで2枚の写真を移動して見せる
+        breakpoint: 426,//モニターの横幅が426px以下の見せ方
+        settings: {
+          slidesToShow: 1,//スライドを画面に1枚見せる
+          slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+        }
       }
-    },
-    {
-      breakpoint: 426,//モニターの横幅が426px以下の見せ方
-      settings: {
-        slidesToShow: 1,//スライドを画面に1枚見せる
-        slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
-      }
-    }
-  ]
+    ]
 });
 
 
